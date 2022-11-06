96c7d4945568bbf030657591c46172fe7e01e707
nicolaslag2022 <nick.goforth@gmail.com>
2022-11-06 14:21:33 -0600

    week2HW

--- index.js
+++ index.js
@@ -1,13 +1,1 @@
-let pizzaPlace = "Pizza House";
-let numberOfToppings = 11;
-console.log(pizzaPlace, numberOfToppings);
 
-console.log(`"Welcome to ${pizzaPlace} we have ${numberOfToppings} toppings!"`);
-
-if (numberOfToppings > 11){
-  console.log("A whole lot of pizza!")
-}
-
-for (let i=2; i <=10; i+=2) {
-  console.log('count', i)
-}
