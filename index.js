f953ad596f49d9104c80ed64fe312a3f97e82065
nicolaslag2022 <nick.goforth@gmail.com>
2022-11-06 14:05:51 -0600

    WK 2 HW

---
+++ index.js
@@ -0,0 +1,13 @@
+let pizzaPlace = "Pizza House";
+let numberOfToppings = 11;
+console.log(pizzaPlace, numberOfToppings);
+
+console.log(`"Welcome to ${pizzaPlace} we have ${numberOfToppings} toppings!"`);
+
+if (numberOfToppings > 11){
+  console.log("A whole lot of pizza!")
+}
+
+for (let i=2; i <=10; i+=2) {
+  console.log('count', i)
+}
