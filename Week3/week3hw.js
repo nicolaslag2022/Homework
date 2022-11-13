fc225dbc6daf277bc2b2b14145cdffef95d88a51
nicolaslag2022 <nick.goforth@gmail.com>
2022-11-13 13:35:05 -0600

    wk 3 hw

--- Week3/week3hw.js
+++ Week3/week3hw.js
@@ -1,7 +1,8 @@
 
-
+//1
 let pizzaToppings = ["pepperoni", "sausage", "ham", "pineapple"];
 
+//2
 function greetCustomer () {
   console.log("Welcome to pizza house");
   for (let topping of pizzaToppings) {
@@ -19,4 +20,35 @@ function getPizzaOrder (size, crust, ...Toppings){
 }
 console.log("One small thin crust pepperoni pizza coming up");
 return console.log([size, crust, Toppings]);
+
+//4
+function preparePizza ([size, crust, toppings]){
+  let pizza = {
+    size: "Large",
+    crust: "Thin",
+    toppings: "Pineapple",
+  }
+  console.log("Cooking...")
+return pizza;
+};
+preparePizza("large", "thin", "pepperoni");
+
+//5
+function servePizza(pizza); {
+  console.log("Order up! Here's your large thick crust pizza with ${pizza.size} ${pizza.crust ${pizza.toppings}");
+  return pizza;
+};
+
+servePizza(preparePizza)(getPizzaOrder("large", "thin crust", "pepperoni", "sausage", "ham", "pineapple"));
+
+
+
+
+
+
+function pizzaToppings(ham, sausage) {
+  console.log(ham);
+  console.log(sausage)
+}
+pizzaToppings()
 
