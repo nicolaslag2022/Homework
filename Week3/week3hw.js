49873b49dc36f16734ab15896d1963374e0aa6cd
nicolaslag2022 <nick.goforth@gmail.com>
2022-11-17 22:04:32 -0600

    week4hw

--- Week3/week3hw.js
+++ Week3/week3hw.js
@@ -5,50 +5,44 @@ let pizzaToppings = ["pepperoni", "sausage", "ham", "pineapple"];
 //2
 function greetCustomer () {
   console.log("Welcome to pizza house");
-  for (let topping of pizzaToppings) {
-    console.log(topping);
+  for (topping of pizzaToppings) {
+    console.log(`${topping});
   }
-}
+};
+greetCustomer();
+
+//3
+
+function getPizzaOrder (size, crust, ...toppings) {
+  let orderUp = `One ${size} ${crust} crust pizza with ${toppings} coming up! `;
+  for (let topping of toppings) {
+    orderUp += `${topping}`;
+  }
+};
+
+//4
+
+function preparePizza ([size, crust, ....restToppings]) {
+  console.log("Now cooking pizza")
+  return {
+    size: size,
+    crust: crust,
+    toppings: toppings
+  }
+};
+
+preparePizza(getPizzaOrder("large", "thin", "pepperoni");
+
+//5
+
+function servePizza(cookingPizza) {
+  console.log(`Order up! one ${pizza.size} ${pizza.crust} curst pizza with ${pizzatoppings}!`);
+  return cookingPizza;
+};
 
 greetCustomer();
 
-function getPizzaOrder (size, crust, ...Toppings){
-  console.log("One large thin crust pizza with pepperoni, ham, and pineapple.")
-  let size = [small, medium, large];
-  let crust = [thin, thick, original];
-  let Toppings = pizzaToppings;
-}
-console.log("One small thin crust pepperoni pizza coming up");
-return console.log([size, crust, Toppings]);
-
-//4
-function preparePizza ([size, crust, toppings]){
-  let pizza = {
-    size: "Large",
-    crust: "Thin",
-    toppings: "Pineapple",
-  }
-  console.log("Cooking...")
-return pizza;
-};
-preparePizza("large", "thin", "pepperoni");
-
-//5
-function servePizza(pizza); {
-  console.log("Order up! Here's your large thick crust pizza with ${pizza.size} ${pizza.crust ${pizza.toppings}");
-  return pizza;
-};
-
-servePizza(preparePizza)(getPizzaOrder("large", "thin crust", "pepperoni", "sausage", "ham", "pineapple"));
+servePizza(preparePizza)(getPizzaOrder("large", "thin crust", "pepperoni", "sausage", "ham", "pineapple")));
 
 
-
-
-
-
-function pizzaToppings(ham, sausage) {
-  console.log(ham);
-  console.log(sausage)
-}
-pizzaToppings()
 
